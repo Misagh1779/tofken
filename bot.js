f42e010a1e955248c93f7b2d900ae282c52ba437
Misagh1779 <145835742+Misagh1779@users.noreply.github.com>
2025-04-28 01:07:38 +0330

    check

--- bot.js
+++ bot.js
@@ -5,27 +5,27 @@ const token = '7892178079:AAFpdGBprjs378rXa5KK1swzfsxYj0ypy18';
 
 // Create a bot that uses 'polling' to fetch new updates
 
-const regex = /^\/start$/
-
-
 const bot = new TelegramBot(token, {polling: true});
+
+const regex = /^\/start$/
 
 bot.on('message', (msg) => {
     let tarif=false;
     const chatId = msg.chat.id;
 
-    const userText=msg.text;
+    const userText = msg.text;
+
     if(userText == '/start'){
-        let tarif=true;
-        bot.sendMessage(chatId,"به توفکن خوش اومدی خوشتیپ!");
+        let tarif = true;
+        bot.sendMessage(chatId," به توفکن خوش اومدی خوشتیپ!");
     }
    
     if(userText == 'salam'){
-        let tarif=true;
-        bot.sendMessage(chatId,"چخبر خوشتیپ");
+        let tarif = true;
+        bot.sendMessage(chatId, "چخبر خوشتیپ");
     }
-if(!tarif) {
-    bot.sendMessage(chatId,"چی چی بیه؟");
+    if(!tarif) {
+    bot.sendMessage(chatId, "چی چی بیه؟");
 }
 
 });
