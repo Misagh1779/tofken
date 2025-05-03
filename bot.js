2920b7f6592b272694eafd9928add6a4fec456eb
Misagh1779 <145835742+Misagh1779@users.noreply.github.com>
2025-05-03 18:53:51 +0330

    refix

--- bot.js
+++ bot.js
@@ -1,25 +1,48 @@
 const TelegramBot = require('node-telegram-bot-api');
 
+// جایگزین توکن با توکن خودت
 const token = '7892178079:AAFpdGBprjs378rXa5KK1swzfsxYj0ypy18';
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 const regex = /^\/start$/;
 
 bot.on('message', (msg) => {
-    let tarif = false; // تعریف فقط یکبار اینجا
-    
+    let tarif = false;
     const chatId = msg.chat.id;
     const userText = msg.text;
 
-    if (regex.test(userText)) {  // از regex استفاده کن
-        tarif = true;  // فقط مقدار بدیم، نه let جدید
-        bot.sendMessage(chatId, "به توفکن خوش اومدی خوشتیپ!");
+    if (userText === '/start') {
+        tarif = true;
+        bot.sendMessage(chatId, "به توفکن خوش اومدی خوشتیپ!", {
+            reply_markup: {
+                keyboard: [
+                    ["سلام", "بای"],
+                    ["تست1", "تست2"]
+                ],
+                resize_keyboard: true
+            }
+        });
     }
 
-    if (userText === 'salam') {
+    if (userText === 'سلام') {
         tarif = true;
         bot.sendMessage(chatId, "چخبر خوشتیپ");
+    }
+
+    if (userText === 'بای') {
+        tarif = true;
+        bot.sendMessage(chatId, "خودتم بای");
+    }
+
+    if (userText === 'تست1') {
+        tarif = true;
+        bot.sendMessage(chatId, "تست بیر");
+    }
+
+    if (userText === 'تست2') {
+        tarif = true;
+        bot.sendMessage(chatId, "تست ایکی");
     }
 
     if (!tarif) {
@@ -28,3 +51,4 @@ bot.on('message', (msg) => {
 });
 
 console.log("bot is started");
+
