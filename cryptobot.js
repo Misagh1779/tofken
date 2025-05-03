61f69339244eac50dce5386d43bcbdfe0a1b94ad
Misagh1779 <145835742+Misagh1779@users.noreply.github.com>
2025-05-03 20:50:24 +0330

    tst2

--- cryptobot.js
+++ cryptobot.js
@@ -1,22 +1,27 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '7892178079:AAFpdGBprjs378rXa5KK1swzfsxYj0ypy18';
 const bot = new TelegramBot(token, { polling: true });
-const { default:axios } = require('axios');
+const { default: axios } = require('axios');
 
-bot.on("text",async (msg)=>{
-    const chatId=msg.chat.id;
+bot.on("text", async (msg) => {
+    const chatId = msg.chat.id;
     const userMessage = msg.text;
     let notcontrollermessage = true;
 
-    if (userMessage == "/start") {
+    if (userMessage === "/start") {
         notcontrollermessage = false;
-bot.sendMessage(chatId, 'به ربات قیمت لحظه ای توفکن خوش اومدی خوشتیپ!')
-
-
+        bot.sendMessage(chatId, 'به ربات قیمت لحظه‌ای توفکن خوش اومدی خوشتیپ!', {
+            reply_markup: {
+                remove_keyboard: true // ✅ دکمه‌ها پاک می‌شن
+            }
+        });
     }
-
 
     if (notcontrollermessage) {
-        bot.sendMessage(chatId, 'از دستورات موجود استفاده کن!');
+        bot.sendMessage(chatId, 'از دستورات موجود استفاده کن!', {
+            reply_markup: {
+                remove_keyboard: true // ✅ حتی برای پیام‌های دیگه هم پاک بشه
+            }
+        });
     }
-})
+});
