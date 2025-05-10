3aa6d1391fa03fd584294a9a821e7f7a2715c043
Misagh1779 <145835742+Misagh1779@users.noreply.github.com>
2025-05-10 19:04:31 +0330

    fixingthisshit

--- cryptobot.js
+++ cryptobot.js
@@ -1,52 +1,46 @@
-const token = '7892178079:AAFpdGBprjs378rXa5KK1swzfsxYj0ypy18';
+const token = 'توکن واقعی‌تو بذار اینجا';
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, { polling: true });
 const { default: axios } = require('axios');
-let SymbolsMessage="";
 
-    async function getSymbolsListMessage(){
-        SymbolsMessage="";
-        const response= await axios.get("https://api.nobitex.net/v2/orderbook/all");
-    
-    for (const symbol in response.data){
-        SymbolsMessage += `${symbol }
-        
-        `
-      }
-  console.log("Symbols fetched")
-  return SymbolsMessage
+async function getSymbolsListMessage() {
+    let SymbolsMessage = "";
+    const response = await axios.get("https://api.nobitex.net/v2/orderbook/all");
+
+    for (const symbol in response.data) {
+        SymbolsMessage += `${symbol}\n`;
+    }
+
+    console.log("Symbols fetched");
+    return SymbolsMessage;
 }
 
-getSymbolsListMessage()
-    
-        bot.on("text", async (msg) => {
-      const chatId = msg.chat.id;
+bot.on("text", async (msg) => {
+    const chatId = msg.chat.id;
     const userMessage = msg.text;
     let notcontrollerMessage = true;
 
     if (userMessage === "/start") {
-          notcontrollerMessage = false;
-          bot.sendMessage(chatId, 'به ربات قیمت لحظه‌ای توفکن خوش اومدی خوشتیپ!',{
-              reply_markup: {
+        notcontrollerMessage = false;
+        bot.sendMessage(chatId, 'به ربات قیمت لحظه‌ای توفکن خوش اومدی خوشتیپ!', {
+            reply_markup: {
                 keyboard: [
-            [{ text: "لیست نمادها" }]
+                    [{ text: "لیست نمادها" }]
                 ],
                 resize_keyboard: true,
                 one_time_keyboard: false
-
             }
         });
-      }
-
-      if (userMessage=="لیست نمادها"){
-        notcontrollerMessage = false;
-        bot.sendMessage(chatId, SymbolsMessage)
-      }
-
-      if (notcontrollerMessage) {
-          bot.sendMessage(chatId, 'از دستورات موجود استفاده کن!'), {
-              
-              }
-    ;
     }
-        });
+
+    if (userMessage === "لیست نمادها") {
+        notcontrollerMessage = false;
+        const SymbolsMessage = await getSymbolsListMessage();
+        bot.sendMessage(chatId, SymbolsMessage);
+    }
+
+    if (notcontrollerMessage) {
+        bot.sendMessage(chatId, 'از دستورات موجود استفاده کن!');
+    }
+});
+
